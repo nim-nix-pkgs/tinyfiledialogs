7241996778730efbf57c0bdae63d1d87ab3b6ce0
hugosenari <hugosenari@gmail.com>
2022-03-12 03:39:35 -0300

    chore: re index tinyfiledialogs

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''TinyFileDialogs for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tinyfiledialogs-master".url = "path:./master";
+  inputs."tinyfiledialogs-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tinyfiledialogs-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''TinyFileDialogs for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tinyfiledialogs-master.flake = false;
+  inputs.src-tinyfiledialogs-master.owner = "juancarlospaco";
+  inputs.src-tinyfiledialogs-master.ref   = "refs/heads/master";
+  inputs.src-tinyfiledialogs-master.repo  = "nim-tinyfiledialogs";
+  inputs.src-tinyfiledialogs-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tinyfiledialogs-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tinyfiledialogs-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
